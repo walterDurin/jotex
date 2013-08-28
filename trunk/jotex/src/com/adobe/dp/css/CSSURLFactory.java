c56d377dd50f8aeb684d309d164cedf4dd59c50f
luca.conte <luca.conte@697716b4-2d15-b8cf-b856-06d7f69b5942>
2013-08-28 10:15:28 +0000

    mvn switching ops

--- trunk/jotex/src/com/adobe/dp/css/CSSURLFactory.java
+++
@@ -1,6 +0,0 @@
-package com.adobe.dp.css;
-
-public interface CSSURLFactory {
-
-	CSSURL createCSSURL(String url);
-}
--- trunk/jotex/src/com/adobe/dp/epub/conv/Version.java
+++
@@ -1,5 +0,0 @@
-package com.adobe.dp.epub.conv;
-
-public class Version {
-	public static final String VERSION = "0.5.0";
-}
